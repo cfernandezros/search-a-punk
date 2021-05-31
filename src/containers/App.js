f2655c287f87dda7eb91ebf852c0f76509252ce8
Carlos Fernández Rosauro <yogg.carlos@gmail.com>
2021-05-31 21:10:03 +0200

    Added a refresh button

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,17 +9,18 @@ function App() {
 	const [punks, setPunks] = useState([])
 	const [searchfield, setSearchfield] = useState('')
 
-	useEffect (() => {
-		fetch('https://cryptopunks.herokuapp.com/api/punks/')
+	const fetchData = async ()=> {
+		await fetch('https://cryptopunks.herokuapp.com/api/punks/')
 		.then(response => response.json())
 		.then(users => setPunks(users.sort(() => 
-			0.5 - Math.random()).slice(0, 20)));
-	}, [])
+			0.5 - Math.random()).slice(0, 20)))};
+	
+	useEffect(() => {fetchData()}, [])
 
 	const onSearchChange = (event) => {
 		setSearchfield(event.target.value)
 	}
-
+	
 	const filteredPunks = punks.filter(punk => {
 		return punk.accessories.toString().toLowerCase().includes(searchfield.toLowerCase())
 	});
@@ -28,8 +29,11 @@ function App() {
 	} else {
 		return (
 		<div className='tc'>
-			<h1 className='f1'>search-a-punk</h1>
-			<SearchBox searchChange={onSearchChange}/>
+		<h1 className='f1'>search-a-punk</h1>
+			<div className="banner">
+				<button onClick={fetchData} className='pa3 mb3 br3 ba'>Refresh Punks!!!</button>
+				<SearchBox searchChange={onSearchChange}/>
+			</div>
 			<Scroll>
 				<CardList punks={filteredPunks} />
 			</Scroll>
