2b908d1511ad7c67b3be5e965343bb35d50ed046
Carlos Fernández Rosauro <yogg.carlos@gmail.com>
2021-05-31 14:11:35 +0200

    Transformed class component into functional component with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/scroll'
 
-class App extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			punks: [],
-			searchfield: ''
-		}
-	}
 
-	componentDidMount() {
+function App() {
+	const [punks, setPunks] = useState([])
+	const [searchfield, setSearchfield] = useState('')
+
+	useEffect (() => {
 		fetch('https://cryptopunks.herokuapp.com/api/punks/')
 		.then(response => response.json())
-		.then(users => this.setState({ punks: users.sort(() => 
-			0.5 - Math.random()).slice(0, 20) }));
+		.then(users => setPunks(users.sort(() => 
+			0.5 - Math.random()).slice(0, 20)));
+	}, [])
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value)
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
-
-	render() {
-		const { punks, searchfield } = this.state
-		const filteredPunks = punks.filter(punk => {
-			return punk.accessories.toString().toLowerCase().includes(searchfield.toLowerCase())
-		});
-		if (!punks.length) {
-			return <h1 className='tc'>Loading...</h1>
-		} else {
-			return (
-			<div className='tc'>
-				<h1 className='f1'>search-a-punk</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<Scroll>
-					<CardList punks={filteredPunks} />
-				</Scroll>
-			</div>
-			);
-		}
+	const filteredPunks = punks.filter(punk => {
+		return punk.accessories.toString().toLowerCase().includes(searchfield.toLowerCase())
+	});
+	if (!punks.length) {
+		return <h1 className='tc'>Loading...</h1>
+	} else {
+		return (
+		<div className='tc'>
+			<h1 className='f1'>search-a-punk</h1>
+			<SearchBox searchChange={onSearchChange}/>
+			<Scroll>
+				<CardList punks={filteredPunks} />
+			</Scroll>
+		</div>
+		);
 	}
 }
 
